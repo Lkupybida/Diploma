f12fa2243a4d262a0c81dc5d9fa6fc2e1fc424d2
Lev <lkupybida@gmail.com>
2026-05-06 23:28:29 +0300

    last training efforts which I forgot to commit

--- train_models_v2/common_settings.py
+++ train_models_v2/common_settings.py
@@ -18,10 +18,19 @@ HORIZON = 48
 GLOBAL_MIN_DT = pd.Timestamp("2024-01-01 01:00:00", tz="Europe/Kyiv")
 
 weather_cols_to_drop = [
-    "apparent_temperature", "rain", "snowfall",
-    "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high",
-    "surface_pressure", "wind_direction_10m", "wind_gusts_10m",
-    "diffuse_radiation", "direct_normal_irradiance",
+       'apparent_temperature', 'dew_point_2m', 'relative_humidity_2m',
+    'rain', 'snowfall', 'cloud_cover_low',
+       'cloud_cover_mid', 'cloud_cover_high', 'surface_pressure',
+    'wind_direction_10m', 'wind_gusts_10m', 'diffuse_radiation',
+]
+
+weather_cols_to_keep = [
+    "temperature_2m",
+    "precipitation",
+    "cloud_cover",
+    "wind_speed_10m",
+    "shortwave_radiation",
+    "direct_normal_irradiance"
 ]
 
 weather_cols_all = ['temperature_2m',
@@ -32,11 +41,11 @@ weather_cols_all = ['temperature_2m',
        'shortwave_radiation', 'diffuse_radiation', 'direct_normal_irradiance']
 
 cat_columns = [
-    'eic_code', 'osr_desc', 'station_type', 'oblast'
+    'eic_code', 'dso_desc', 'station_type', 'oblast'
 ]
 
 static_cols = [
-    'latitude', 'longitude', 'eic_code', 'osr_desc', 'station_type', 'oblast'
+    'latitude', 'longitude', 'eic_code', 'dso_desc', 'station_type', 'oblast'
 ]
 
 calendar_cols = [
